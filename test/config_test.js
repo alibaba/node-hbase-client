c51d5f9ea3a66a985131e4a53c8362c5e0dbe202
fengmk2 <fengmk2@gmail.com>
2014-02-20 10:46:10 +0800

    Support HBase 0.94.16 fixed #36

--- test/config_test.js
+++ test/config_test.js
@@ -1,7 +1,10 @@
-/*!
+/**!
  * node-hbase-client - test/config_test.js
- * Copyright(c) 2013 fengmk2 <fengmk2@gmail.com>
- * MIT Licensed
+ *
+ * Copyright(c) 2014 Alibaba Group Holding Limited.
+ *
+ * Authors:
+ *   苏千 <suqian.yf@taobao.com> (http://fengmk2.github.com)
  */
 
 "use strict";
@@ -11,12 +14,14 @@
  */
 
 var config = {
+  // 0.94.0
   zookeeperHosts: [
     '10.232.98.74',
     '10.232.98.75', '10.232.98.76',
-    '10.232.98.77', '10.232.98.78'
+    '10.232.98.77', '10.232.98.78',
   ],
   zookeeperRoot: '/hbase-rd-test-0.94',
+
   // logger: console,
   logger: {
     warn: function () {},
@@ -25,5 +30,20 @@ var config = {
   },
   rpcTimeout: 40000,
 };
+
+config.clusters = [
+  {
+    zookeeperHosts: config.zookeeperHosts,
+    zookeeperRoot: config.zookeeperRoot,
+  },
+  {
+    zookeeperHosts: [
+      '10.232.98.53:40060',
+      '10.232.98.54:40060',
+      '10.232.98.55:40060',
+    ],
+    zookeeperRoot: '/hbase-0.94.16',
+  }
+];
 
 module.exports = config;
